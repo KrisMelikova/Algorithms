97a0e507c8031d89df22c1615a953dceddabde64
KrisMelikova <kristina@melikova.ru>
2020-11-25 16:28:54 +0300

    Lesson 1 Algorithms

---
+++ Les1Task2.py
@@ -0,0 +1,8 @@
+#Пользователь вводит номер буквы в алфавите. Определить, какая это буква.
+#https://drive.google.com/file/d/1WXaZG1oh7tEwHe_1qV5Ml0TC2qCV-A0J/view?usp=sharing
+
+a = int(input('Введите номер буквы в алфавите в диапазоне чисел с 65 до 90'))
+
+a = chr(a)
+
+print(f'Буква: {a}')
